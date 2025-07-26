d0887dcada124ea7a35362f1f4ba79bc1c5b4b18
Agas2024 <agastin6000@gmail.com>
2025-07-26 15:17:15 +0530

    Initial backend commit

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
   e.preventDefault();
 
   try {
-    const res = await axios.post('http://localhost:5000/api/login', {
+    const res = await axios.post('https://aai-stock-backend.onrender.com/api/login', {
       username,
       password
     });
