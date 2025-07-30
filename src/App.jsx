1816be8a3814c56836fe534deb4f192dc9906647
Agas2024 <agastin6000@gmail.com>
2025-07-30 12:33:22 +0530

    Initial backend commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import Login from './Login/Login';
 import GetStarted from './Pages/GetStarted';
